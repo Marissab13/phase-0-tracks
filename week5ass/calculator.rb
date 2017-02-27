385c073ad720a48c2331cf9ae65dbdbb2504b6d5
Marissab13 <marissab13@gmail.com>
2017-02-26 23:25:57 -0800

    finishing problem solving

--- week5ass/calculator.rb
+++ week5ass/calculator.rb
@@ -1,23 +1,20 @@
 
-puts "enter number 1"
-x = gets.chomp.to_i
-puts "enter number 2"
-z = gets.chomp.to_i
-puts "enter which operation to do"
-oper = gets.chomp.to_sym
 
-# def calculate(x, z, oper)
-  ans = case oper 
-        when '+'
-        answer = x + z
-        when '-'
-            answer = x - z
-        when '*'
-            answer = x * z
-        when '/'
-            answer = x / z
-        end
-    puts ans
-    puts answer
-# end
+    puts "Please enter the first number"
+     a = gets.chomp.to_i
+    puts "Please enter the second number"
+      b = gets.chomp.to_i
+    puts "Please enter which operation"
+      operator = gets.chomp
+  
+  
+    if operator == "+"
+      puts= a + b
+    elsif operator == "-"
+      puts = a - b
+    elsif operator == "*"
+      puts = a * b
+    elsif operator == "/"
+      puts = a / b
+    end
 
