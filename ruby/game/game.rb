43ccca85960c80e4efc8c6180984d1be71fb4b67
Marissab13 <marissab13@gmail.com>
2017-03-10 14:57:28 -0800

    finishing solo challenge

--- ruby/game/game.rb
+++ ruby/game/game.rb
@@ -1,51 +1,63 @@
 class DragonGame
-	attr_reader 
-	attr_writer 
+	attr_accessor :name, :letter_guess
 
 
-	def initialize_1
+	def initialize
 		@guesses = 0
 		@over = false
-		@name_array = ["alexstraza", "nozdormu", "yesera", "malygos", "neltharion", "onyxia", "kalecgos", "galakrond", "deathwing"]
-		@dragon_name = @name_array.sample
-		@dragon_name = @dragon_name.chars
-		
-	
 		puts "Initializing DragonGame instance"
-		puts @dragon_name 
+		puts "Please enter a dragon name to begin. Here are a few you can choose: alexstraza, nozdormu, yesera, malygos, neltharion, onyxia, kalecgos, galakrond, and deathwing"
 		end 
 
-		def dragon_name
-			@dragon_name = @name_array.sample
-			@dragon_name = @dragon_name.chars
+		def dragon_name(name)
+			dragon_name = name.chars
 		end
 
 		def user_guess
-			puts "Please enter a letter"
+			puts "Now, have your buddy enter a letter! Can he guess your dragon's name?"
 			@letter_guess = gets.chomp.downcase 
-
-			if @dragon_name.include?(@letter_guess)
-				puts "You have guessed a letter correctly!"
-			else
-				@guesses += 1
-				puts "You are incorrect, please try again."
-			end 
 		end 
+		# def show_progress
+		# 	@letter_guess.each do |l|
+  #   		if @name.include? l
+  #     		print l
+  #   		else
+  #     		print '_ '
+  #     		won = false
+  #   		end
+  # 		end
+
+			# if @name.include? @letter_guess
+			# 	puts "You have guessed a letter correctly!"
+			# else
+			# 	@guesses += 1
+			# 	puts "You are incorrect, please try again."
+			# end 
+		
 
 
-		def show_progress
-			for |letter| in @dragon_name 
-				if @letter_guess != |letter|
-					letter = "-"
-				end 
-		end 
+		# def show_progress
+		# @letter_guess.each do |l|
+  #   	if @name.include? l
+  #     	print l
+  #   	else
+  #     	print '_ '
+  #     	won = false
+  #   		end
+  # 		end
+	 
 
 		
 end 
 			
 			
 
-# dragon_game = DragonGame.new
+game = DragonGame.new 
+# dragon_game.dragon_name("Alexstraza")
+# dragon_game.user_guess
+# dragon_game.user_guess
+# dragon_game.show_progress
+
 
 
 
