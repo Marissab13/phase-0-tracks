fe56edf16abaec56fa9defa94d43a3fd2f296b51
Marissab13 <marissab13@gmail.com>
2017-03-13 00:32:31 -0700

    finished algos.js

--- js/algos.js
+++ js/algos.js
@@ -8,22 +8,53 @@
 
 
 //RELEASE 1
+//create a function that takes two objects with keys and values
+//compare the keys and values, check for matches
+//if there are no matches, return false
+
 
 //RELEASE 2
 
 function longestWord(str) {
-
-	var split = str.split(' ');
-
-	var longWord = 0;
-
-	for(var i = 0; i < split.length; i++) {
+    var split = str.split(' ');
+    var longWord = 0;
+    for(var i = 0; i < split.length; i++) {
     if(split[i].length > longestWord.length) { 
-	longestWord = split[i].length; 
-		}
-	}
+    longestWord = split[i].length; 
+        }
+    }
 
 return longestWord;
 
 }
+ //DRIVER CODE
  longestWord("Your muffins smell like shit and so do your ideas.");
+
+
+function valueMatch( obj1, obj2 ) {
+  return Object.keys( obj1 ).every( function( prop ) {
+    return obj2.hasOwnProperty( prop );
+  });
+}
+
+//DRIVER CODE
+
+valueMatch({name: "Steven", age: 54}, {name: "Tamir", age: 54})
+valueMatch({name: "Margaret", age: 20}, {name: "Marky Mark", age: 40})
+
+
+
+function createString(num) {
+    var text = " ";
+
+    var charset = "abcdefghijklmnopqrstuvwxyz0123456789";
+    for (var i=0; i < num; i++ )
+        text += charset.charAt(Math.floor(Math.random() * charset.length));
+
+    return text;
+
+}
+
+console.log(createString(10));
+console.log(createString(4));
+console.log(createString(9));
