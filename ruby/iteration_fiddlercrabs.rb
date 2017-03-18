4a69e9145cd8188d8553f96f450c15729e80231e
Marissab13 <marissab13@gmail.com>
2017-03-18 01:51:43 -0700

    finished iteration exercise 5.3

--- ruby/iteration_fiddlercrabs.rb
+++ ruby/iteration_fiddlercrabs.rb
@@ -14,12 +14,47 @@ pets.each do |pet, name|
 end
 
 
-#using .map! on the array
+# #using .map! on the array
 
 puts animals
 animals.map! do |animal|
 	puts animal.upcase
 end 
 
+#A method that iterates through the items, deleting any that meet a certain condition 
+animals.each do |animal|
+  animals.delete_if { |animal| animal =="porcupine"}
+end 
 
+pets.each do |pet, name|
+  pets.delete_if { |pet| pet =="cat"}
+end 
 
+# A method that filters a data structure for only items that do satisfy a certain condition
+animals.each do |animal|
+  animals.delete_if { |animal| animal !="porcupine"}
+end 
+
+pets.each do |pet, name|
+	pets.delete_if {|pet| pet != "cat"}
+end 
+
+#A different method that filters a data structure for only items satisfying a certain condition
+
+pets.each do |pet|
+pets.cycle(2) { |pet| puts pet } 
+end
+
+animals.each do |animal|
+	animals.cycle(3) {|animal| puts animal}
+end 
+
+#A method that will remove items from a data structure until the condition in the block evaluates to false, then stops
+
+animals.each do |animal|
+animals.delete_if { |animal| animal.length <= 5} 
+end 
+
+pets.each do |pet|
+  pets.delete_if {pets.has_key?("cat")}
+end 
