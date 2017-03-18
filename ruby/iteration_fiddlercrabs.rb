25aeabf54acc0b9b417d966f742d42cd9cc40ca6
Marissab13 <marissab13@gmail.com>
2017-03-18 00:40:17 -0700

    added array and hash examples

--- ruby/iteration_fiddlercrabs.rb
+++ ruby/iteration_fiddlercrabs.rb
@@ -2,3 +2,24 @@ animals = ["porcupine", "platypus", "capybara", "liger"]
 
 pets = {'dog' => 'Daeny', 'cat' => 'Zero', 'horse' => 'Margo', 'turtle' => 'Sheldon'} 
 
+#using .each on the array and the hash
+puts animals
+animals.each do |animal|
+	puts "The #{animal} is a very interesting animal."
+end 
+
+puts pets
+pets.each do |pet, name|
+	puts "This #{pet} is named #{name}."
+end
+
+
+#using .map! on the array
+
+puts animals
+animals.map! do |animal|
+	puts animal.upcase
+end 
+
+
+
