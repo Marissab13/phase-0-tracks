815fd570fe2d9b70900e559fa71c66ebbe4afe7c
Marissab13 <marissab13@gmail.com>
2017-03-18 20:22:23 -0700

    adding main hash, working on nesting

---
+++ ruby/nested_data_structures_fiddlercrabs.rb
@@ -0,0 +1,47 @@
+horror_movies = {
+	movie_one: {
+	movie_name: "Insidious",
+	movie_info: {
+	monsters: 1,
+	main_actors: 4
+				},	
+
+	scariest_moments:[
+		"red face pops out behind character",
+		"scary black monster pointing to the bed-ridden child",
+		"man hiding behind the curtains in the baby's room"
+	]
+	}
+
+	movie_two: {
+	movie_name: "The Conjuring 2",
+	movie_info: {
+	monsters: 2,
+	main_actors: 3
+				},	
+
+	scariest_moments:[
+		"nun coming out of the painting",
+		"little girl wakes up on the ceiling",
+		"old man shows himself in the reflection on the TV"
+	]
+	}
+
+	movie_three: {
+	movie_name: "Insidious",
+	movie_info: {
+	monsters: 1,
+	main_actors: 4
+				},	
+
+	scariest_moments:[
+		"red face pops out behind character",
+		"scary black monster pointing to the bed-ridden child",
+		"man hiding behind the curtains in the baby's room"
+	]
+	}
+
+
+
+
+}
