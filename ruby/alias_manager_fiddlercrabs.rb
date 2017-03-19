3afb3c5e502366da9a31fe834d908fb90bf093bd
Marissab13 <marissab13@gmail.com>
2017-03-19 02:07:39 -0700

    adding new alias_manager_fiddlercrabs file

---
+++ ruby/alias_manager_fiddlercrabs.rb
@@ -0,0 +1,5 @@
+#RELEASE 0 
+#Get name input from user
+#swap the first and last name
+#change all the vowels to the next vowel in 'aeiou'
+#change all the consonants to the next consonat in the alphabet
