648b581302d1d3bf6de60ffb570f55ff683f0153
Marissab13 <marissab13@gmail.com>
2017-03-18 20:50:09 -0700

    working on adding more nested data

--- ruby/nested_data_structures_fiddlercrabs.rb
+++ ruby/nested_data_structures_fiddlercrabs.rb
@@ -1,47 +1,50 @@
 horror_movies = {
 	movie_one: {
-	movie_name: "Insidious",
+	movie_name: "House of 1000 Corpses",
 	movie_info: {
-	monsters: 1,
+	monsters: "Captain Spaulding",
+			  "Mother Firefly",
+			  "Baby Firefly",
+			  "Ottis",
+			  "Dr. Satan"
 	main_actors: 4
 				},	
 
 	scariest_moments:[
-		"red face pops out behind character",
-		"scary black monster pointing to the bed-ridden child",
-		"man hiding behind the curtains in the baby's room"
+		"The moment when Otis shows his Fishboy sculpture",
+		"The first appearance of Dr. Satan as he's operating on Jerry.",
+		"The madman-monster that chases Denise through the underground lair of Dr. Satan."
 	]
 	}
 
 	movie_two: {
 	movie_name: "The Conjuring 2",
 	movie_info: {
-	monsters: 2,
+	monsters: "The Old Man",
+			  "The Nun (Valek)"
 	main_actors: 3
 				},	
 
 	scariest_moments:[
-		"nun coming out of the painting",
-		"little girl wakes up on the ceiling",
-		"old man shows himself in the reflection on the TV"
+		"The nun coming out of the painting.",
+		"The moment when little girl wakes up on the ceiling.",
+		"When the old man shows himself in the reflection on the TV."
 	]
 	}
 
 	movie_three: {
 	movie_name: "Insidious",
 	movie_info: {
-	monsters: 1,
+	monsters: "The Demon"
 	main_actors: 4
 				},	
 
 	scariest_moments:[
-		"red face pops out behind character",
-		"scary black monster pointing to the bed-ridden child",
-		"man hiding behind the curtains in the baby's room"
+		"A red face pops out behind one of the main characters.",
+		"A scary black monster pointing to the bed-ridden child.",
+		"The man hiding behind the curtains in the baby's room, attempting to steal the baby."
 	]
 	}
-
-
-
-
 }
+
+p horror_movies[:movie_three][:scariest_moments][0]
