f509e4d4539f89a8756fe0d2bb6af2935dab9f27
Marissab13 <marissab13@gmail.com>
2017-03-19 02:21:24 -0700

    working on release 0 for alias_manager

--- ruby/alias_manager_fiddlercrabs.rb
+++ ruby/alias_manager_fiddlercrabs.rb
@@ -1,5 +1,21 @@
 #RELEASE 0 
 #Get name input from user
 #swap the first and last name
+#change name to an array so a block can be run on each character
+#write a method to find all of the vowels in the string
 #change all the vowels to the next vowel in 'aeiou'
 #change all the consonants to the next consonat in the alphabet
+
+
+def name_swap(first_name, last_name)
+	switched_name =  (last_name + ' ' +first_name).chars
+	puts switched_name
+	switched_name.map do |letter|
+		if 
+	end
+	end 
+
+
+
+name_swap("marissa", "berrong")
+
