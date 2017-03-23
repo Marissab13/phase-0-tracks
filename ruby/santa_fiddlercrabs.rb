e70ea8708ba19dd361c85d6a606fe1e48a61fd60
Marissab13 <marissab13@gmail.com>
2017-03-23 01:03:42 -0700

    finishing santas 6.3

--- ruby/santa_fiddlercrabs.rb
+++ ruby/santa_fiddlercrabs.rb
@@ -1,26 +1,22 @@
 class Santa
 
+	attr_reader  :ethnicity
+	attr_accessor :gender, :age
+
 	def initialize(gender, ethnicity)
 		puts "Initializing Santa instance..."
 		@gender = gender
 		@ethnicity = ethnicity
 		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
-		@age = 0
+		@age = rand(0..140)
 		
 	end 
+
 
 	def speak
 		puts "Ho, ho, ho! Haaaaapy holidays!"
 	end 
 
-	def age
-		@age
-	end 
-
-	def ethnicity
-		@ethnicity
-	end 
-	
 
 	def eats_milk_and_cookies(cookie)
 		puts "That was a good #{cookie}!"
@@ -40,8 +36,16 @@ end
 # santa.eats_milk_and_cookies("Lorna Doone")
 # santa.speak
 santas = []
+
 example_genders = ["cisgender", "female", "transgender", "male", "female", "gender fluid", "foliagender-(those who identify as plants"]
 example_ethnicities = ["xenomorph", "wood elf", "furby", "cenobyte", "unknown", "stormtrooper", "ghost"]
-example_genders.length.times do |i|
-  santas << Santa.new(example_genders[i], example_ethnicities[i])
+
+
+
+50.times do |i|
+  santas << Santa.new(example_genders.sample, example_ethnicities.sample)
+  santas[i].age = rand(0..140)
+  puts "Santa # #{i+1} is a #{santas[i].gender}, #{santas[i].ethnicity}, #{santas[i].age} years old"
 end
+
+
