74c2af486590355644a2df81197bc17016ff8e2c
Marissab13 <marissab13@gmail.com>
2017-03-22 23:40:01 -0700

    Release 0 for 6.3

--- ruby/santa_fiddlercrabs.rb
+++ ruby/santa_fiddlercrabs.rb
@@ -1,2 +1,18 @@
 class Santa
+
+	def initialize
+		puts "Initializing Santa instance..."
+	end 
+
+	def speak
+		puts "Ho, ho, ho! Haaaaapy holidays!"
+	end 
+
+	def eats_milk_and_cookies(cookie)
+		puts "That was a good #{cookie}!"
+	end 
 end 
+
+santa = Santa.new
+santa.eats_milk_and_cookies("Lorna Doone")
+santa.speak
