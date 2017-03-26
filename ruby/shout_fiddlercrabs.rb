fc30a8fbe4545c3a04eea1c33b2538cb4f5abeef
Marissab13 <marissab13@gmail.com>
2017-03-26 00:25:28 -0700

    commented out old code, rewrote shout module and added classes

--- ruby/shout_fiddlercrabs.rb
+++ ruby/shout_fiddlercrabs.rb
@@ -1,12 +1,30 @@
-module Shout
-  def self.yell_angrily(words)
-    words + "!!!" + " :("
-  end
+# module Shout
+#   def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
 
-def yell_happily(words)
-		words + "!!!" + "MAXIMUM EFFORT"
+# def yell_happily(words)
+# 		words + "!!!" + "MAXIMUM EFFORT"
+# 	end 
+# end 
+
+module Shout
+	def yell_angrily(words)
+		words.upcase + "$%@#%!!"
 	end 
 
-
+	def yell_happily(words)
+		words + "!!" + "^ ^"
+	end 
 
 end 
+
+class SillyZebra
+	include Shout
+end 
+
+class DrunkCollegeKid
+	include Shout
+end 
+
+
