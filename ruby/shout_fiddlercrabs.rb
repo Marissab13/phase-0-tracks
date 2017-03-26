1d5488990db981bf332519b39049a54fcf7aa457
Marissab13 <marissab13@gmail.com>
2017-03-26 00:31:28 -0700

    made new instances of the classes and tested with driver code

--- ruby/shout_fiddlercrabs.rb
+++ ruby/shout_fiddlercrabs.rb
@@ -10,11 +10,11 @@
 
 module Shout
 	def yell_angrily(words)
-		words.upcase + "$%@#%!!"
+		words.upcase + " !$%@#%!!"
 	end 
 
 	def yell_happily(words)
-		words + "!!" + "^ ^"
+		words.upcase + "!!" + "^ ^"
 	end 
 
 end 
@@ -26,5 +26,22 @@ end
 class DrunkCollegeKid
 	include Shout
 end 
+
+zebra = SillyZebra.new
+puts zebra.yell_happily("I love basking in the sun in the veldt")
+puts zebra.yell_angrily("Lions can suck it")
+
+kid = DrunkCollegeKid.new
+puts kid.yell_happily("The Dave Matthews Band is my fave jam band")
+puts kid.yell_angrily("Who took my clothes and made me wear a toga?")
+
+
+
+
+
+
+
+
+
 
 
