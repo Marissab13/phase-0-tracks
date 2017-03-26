1869a109fdbbf66970ff79cf7b911867479832a1
Marissab13 <marissab13@gmail.com>
2017-03-26 01:35:31 -0700

    writing methods for game

---
+++ ruby/game/game_fiddlercrabs.rb
@@ -0,0 +1,26 @@
+class WordGame
+
+def initialize
+end 
+	
+
+def get_word(word)
+	end 
+
+def number_of_guesses
+	end 
+
+def guess_counter
+end 
+
+def get_letter(letter)
+	end 
+
+def end_game_message
+end 
+
+
+
+
+
+end 
