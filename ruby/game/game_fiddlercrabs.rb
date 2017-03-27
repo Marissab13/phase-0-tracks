1b6305842674f871efd97c96e4843ec6aec4a247
Marissab13 <marissab13@gmail.com>
2017-03-26 19:40:59 -0700

    added a hide_word method and tested it with driver code

--- ruby/game/game_fiddlercrabs.rb
+++ ruby/game/game_fiddlercrabs.rb
@@ -2,14 +2,16 @@ class WordGame
 
 attr_accessor :word, :guessed, :number_of_guesses
 
-def initialize
-	@word = word@
+def initialize(word)
+	@word = word
 	@number_of_guesses = word.length
 	@guesses_used = []
 	@guessed = []
 end
 
 def hide_word
+	length = @word.length
+	@guessed = @guessed.push('_') * length
 	end 
 
 def user_guesses
@@ -21,6 +23,9 @@ end
 def current_guess
 	end 
 
-	
+
 
 end
+
+word_one = WordGame.new("happy")
+puts word_one.hide_word
