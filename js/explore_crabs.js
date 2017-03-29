127a9bb01816c20cbc739307580003e1843121a8
Marissab13 <marissab13@gmail.com>
2017-03-29 00:38:44 -0700

    wrote pseudocode for release 4

---
+++ js/explore_crabs.js
@@ -0,0 +1,7 @@
+
+// start by creating a function that takes a string as a parameter
+// split up the string so they are indivicual charaters
+// change the set of characters into an array 
+// iterate over the characters and reverse them
+// change the array back into a string
+// call the function 
